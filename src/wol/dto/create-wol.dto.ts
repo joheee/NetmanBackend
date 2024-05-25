451e9d3361f0709973113fb76a4da0636268a2af
Johevin Blesstowi <BS22-1@slc.net>
2024-05-25 17:40:59 +0700

    testing wol

--- src/wol/dto/create-wol.dto.ts
+++ src/wol/dto/create-wol.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateWolDto {
-    @ApiProperty({default:"20:DE:20:DE:20:DE"})
+    @ApiProperty({default:"10:E7:C6:0A:68:09"})
     mac:string
 }
